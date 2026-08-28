b747546cd7e974db839aa07f2e9b22c57d63de2e
VENNELA GOPICHAND <vennelagopichandpandu@gmail.com>
2026-08-28 11:17:36 +0530

    Added GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -12,9 +12,9 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
 
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
